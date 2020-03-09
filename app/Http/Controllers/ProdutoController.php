8f5792e6ea5cac0cc5d5e9e86b5dbd75b19cfde3
João Silva <Joao.Silva@sinsolution.local>
2020-03-09 14:51:16 -0300

    Iniciando Projeto Estoque Loja

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+class ProdutoController extends Controller
+{
+    public function lista()
+    {
+        return '<h1>Lista de Produtos</h1>';
+    }
+}
